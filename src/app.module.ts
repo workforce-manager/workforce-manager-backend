6b140674711e1de5f63715b90a75628f2c36aa49
yuriivintoniak <you@example.com>
2025-07-07 19:57:03 +0300

    chore: configure JWT module using async options and ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { UsersModule } from "./users/users.module";
 import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
---
+++ src/config/jwt.config.ts
@@ -0,0 +1,12 @@
+import { ConfigService } from "@nestjs/config";
+import { JwtModuleAsyncOptions } from "@nestjs/jwt";
+
+export const jwtConfig: JwtModuleAsyncOptions = {
+  inject: [ConfigService],
+  useFactory: async (configService: ConfigService) => ({
+    secret: configService.get<string>("JWT_SECRET"),
+    signOptions: {
+      expiresIn: configService.get<string>("JWT_ACCESS_TOKEN_TTL"),
+    },
+  }),
+};
