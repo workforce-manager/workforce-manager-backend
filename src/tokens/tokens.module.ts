4c38415343ebbdb12924a9c252269b3d525b09c7
yuriivintoniak <you@example.com>
2025-07-13 20:39:55 +0300

    feat: add token refresh logic with validation

--- src/tokens/tokens.module.ts
+++ src/tokens/tokens.module.ts
@@ -2,12 +2,13 @@ import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { TokensService } from "./tokens.service";
 import { jwtConfig } from "src/config/jwt.config";
+import { PrismaService } from "src/prisma.service";
 
 @Module({
   imports: [
     JwtModule.registerAsync(jwtConfig),
   ],
-  providers: [TokensService],
+  providers: [TokensService, PrismaService],
   exports: [TokensService],
 })
 export class TokensModule {}
