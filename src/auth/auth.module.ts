2529f290dae41c3c56ca0a4447e6b854c8e57785
yuriivintoniak <you@example.com>
2025-07-15 18:59:47 +0300

    feat: add JWT strategy for access token validation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,14 @@
 import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { PassportModule } from "@nestjs/passport";
 import { PrismaService } from "src/prisma.service";
 import { AuthController } from "./auth.controller";
+import { JwtStrategy } from "./strategies/jwt.strategy";
 import { TokensModule } from "src/tokens/tokens.module";
 
 @Module({
-  imports: [TokensModule],
+  imports: [TokensModule, PassportModule],
   controllers: [AuthController],
-  providers: [AuthService, PrismaService],
+  providers: [AuthService, PrismaService, JwtStrategy],
 })
 export class AuthModule {}
--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,4 +1,5 @@
 export const AUTH_ERRORS = {
+  USER_NOT_FOUND: "User does not exist",
   EMAIL_ALREADY_EXISTS: "Email is already in use",
   INVALID_CREDENTIALS: "Invalid email or password",
   INVALID_REFRESH_TOKEN: "Invalid or expired refresh token",
